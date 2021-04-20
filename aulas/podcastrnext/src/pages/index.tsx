5c23a4f7112dd43ff0e93c22c3912f2e9d49de16
Lucas da Luz <lucasdaluz0399@gmail.com>
2021-04-20 20:15:26 -0300

    ssg

--- aulas/podcastrnext/src/pages/index.tsx
+++ aulas/podcastrnext/src/pages/index.tsx
@@ -1,5 +1,21 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 
-export default function Home() {
-  return <Fragment />;
+export default function Home({ episodes }) {
+  return (
+    <div>
+      <p>{JSON.stringify(episodes)}</p>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
 }
